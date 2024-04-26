e3c9adeb589fadd7750c179eebadef2c0c1ffae9
Mina Morgan <minammorgan21@gmail.com.com>
2024-04-26 03:08:01 +0300

    Lab 7 finished

---
+++ Lab 7 (Queues in FreeRTOS)/Code/Objects/main.d
@@ -0,0 +1,17 @@
+./objects/main.o: main.c D:\programs\Keil5\ARM\ARMCLANG\include\stdint.h \
+  D:\programs\Keil5\ARM\ARMCLANG\include\stdio.h \
+  C:\Users\Mark\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.5.1\Source\include\FreeRTOS.h \
+  D:\programs\Keil5\ARM\ARMCLANG\include\stddef.h \
+  RTE\RTOS\FreeRTOSConfig.h \
+  C:\Users\Mark\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.5.1\CMSIS\RTOS2\FreeRTOS\Include\freertos_evr.h \
+  D:\programs\Keil5\ARM\ARMCLANG\include\string.h \
+  RTE\_Target_1\RTE_Components.h \
+  C:\Users\Mark\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.5.1\Source\include\projdefs.h \
+  C:\Users\Mark\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.5.1\Source\include\portable.h \
+  C:\Users\Mark\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.5.1\Source\include\deprecated_definitions.h \
+  C:\Users\Mark\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.5.1\Source\portable\GCC\ARM_CM4F\portmacro.h \
+  C:\Users\Mark\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.5.1\Source\include\mpu_wrappers.h \
+  C:\Users\Mark\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.5.1\Source\include\task.h \
+  C:\Users\Mark\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.5.1\Source\include\list.h \
+  tm4c123gh6pm.h DIO.h std_types.h \
+  C:\Users\Mark\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.5.1\Source\include\queue.h
